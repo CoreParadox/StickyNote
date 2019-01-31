727299d17966fa781a0b0cfd532f5477e49ef652
CoreParadox <6187665+CoreParadox@users.noreply.github.com>
2019-01-31 07:23:56 -0600

    initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+<div id="home">
+    <NavBar></NavBar>
+    <Editor v-on:finished="handleClose()"></Editor>
+</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import Editor from '@/components/Editor.vue'; // @ is an alias to /src
+import NavBar from '@/components/NavBar.vue'
+
+@Component({
+  components: {
+    Editor,
+    NavBar
+  }
+})
+export default class Home extends Vue {
+  handleClose(){
+    console.log("Closing");
+    const remote = require('electron').remote;
+    let w = remote.getCurrentWindow();
+    w.close();
+  }
+}
+</script>
