9bc48ae7ab9809738ed942c8ba2f09fad6972c10
CoreParadox <6187665+CoreParadox@users.noreply.github.com>
2019-02-04 14:58:50 -0600

    Fixed several errors (mostly startup errors due to unset variables or missing files that get created on first update cycle), Fixed more styling issues, Improved settings display and function, where there are no notes, one is created and set as default. Additional editors should no longer be created due to them not being destroyed

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,28 +1,32 @@
 <template>
   <div id="home">
     <NavBar>
-    <router-link slot="controls" :to="'settings'"><a><i class="fas fa-cog"></i></a></router-link>
-    <input v-on:change="updateFileName" v-model="fileName" type="text" name="fileName"/>
-    <select
-      v-on:change="changeFile"
-      v-model="SelectedFile"
-      id="filePicker"
-      style="margin-left:20px;"
-      name="selectedFile"
-    >
-      <option
-        v-for="file in Files()"
-        :value="file.value"
-        :key="file.id"
-        :id="file.id"
-      >{{ file.value }}</option>
-    </select>
-    <a v-on:click="newNote()">
-      <i class="fas fa-file-medical"></i>
-    </a>
-    <a v-on:click="deleteNote()">
-      <i class="fas fa-trash"></i>
-    </a>
+      <router-link slot="controls" :to="'settings'">
+        <a>
+          <i class="fas fa-cog"></i>
+        </a>
+      </router-link>
+      <input v-on:change="updateFileName" v-model="fileName" type="text" name="fileName">
+      <select
+        v-on:change="changeFile"
+        v-model="SelectedFile"
+        id="filePicker"
+        style="margin-left:20px;"
+        name="selectedFile"
+      >
+        <option
+          v-for="file in Files()"
+          :value="file.value"
+          :key="file.id"
+          :id="file.id"
+        >{{ file.value }}</option>
+      </select>
+      <a v-on:click="newNote()">
+        <i class="fas fa-file-medical"></i>
+      </a>
+      <a v-on:click="deleteNote()">
+        <i class="fas fa-trash"></i>
+      </a>
     </NavBar>
     <Editor v-on:finished="handleClose()" v-on:fileLoaded="setFileName"></Editor>
   </div>
@@ -32,7 +36,7 @@
 import { Component, Vue } from "vue-property-decorator";
 import Editor from "@/components/Editor.vue"; // @ is an alias to /src
 import NavBar from "@/components/NavBar.vue";
-import {unlink, writeFile, readdirSync} from "fs";
+import { unlink, writeFile, readdirSync } from "fs";
 import * as path from "path";
 import Configuration from "@/utility/Configuration";
 const defaultNoteText =
@@ -51,9 +55,19 @@ export default class Home extends Vue {
   private configuration = new Configuration();
 
   Files() {
-    return readdirSync(this.configuration.notePath).map((v, i) => {
-      return { id: i, value: v };
-    });
+    var files;
+    try {
+      files = readdirSync(this.configuration.notePath).map((v, i) => {
+        return { id: i, value: v };
+      });
+    }catch (e) {}
+    if (files && files.length == 0) {
+      this.newNote(c => {
+        this.configuration.defaultNote = c;
+        this.configuration.saveConfig();
+      });
+    }
+    return files;
   }
 
   public mounted() {
@@ -63,44 +77,45 @@ export default class Home extends Vue {
   }
 
   updateFileName() {
-    this.$root.$emit("fileNameUpdated", this.fileName);
+    this.$root.$emit("fileNameUpdated", this.fileName || "");
   }
-  
+
   changeFile() {
-    this.$root.$emit("fileChange", this.SelectedFile);
+    this.$root.$emit("fileChange", this.SelectedFile || "");
   }
-  
-  newNote() {
+
+  newNote(callback?) {
     let FileName = `${Date.now()}.md`;
     writeFile(
       path.join(this.configuration.notePath, FileName),
       defaultNoteText,
       "",
-      _ => {
+      c => {
         this.setFileName(FileName);
         this.changeFile();
+        if (callback) callback(FileName);
       }
     );
   }
 
   deleteNote() {
     confirm(`This will permantly delete "${this.SelectedFile}". Continue?`);
-    unlink(path.join(this.configuration.notePath, this.SelectedFile), _ =>{
-      this.setFileName(this.Files()[0].value)
-      this.changeFile();
+    unlink(path.join(this.configuration.notePath, this.SelectedFile), _ => {
+      if (this.Files().length != 0) {
+        this.setFileName(this.Files()[0].value);
+        this.changeFile();
+      }
     });
   }
 
   setFileName(fileName) {
-    var rawFileName=fileName.replace(/.md$/, "");
-    this.SelectedFile = rawFileName+".md";
-    this.fileName = rawFileName
+    var rawFileName = fileName.replace(/.md$/, "");
+    this.SelectedFile = rawFileName + ".md";
+    this.fileName = rawFileName;
   }
 
   handleClose() {
-    const remote = require("electron").remote;
-    let w = remote.getCurrentWindow();
-    w.close();
+    require("electron").remote.getCurrentWindow().close();
   }
 }
 </script>
